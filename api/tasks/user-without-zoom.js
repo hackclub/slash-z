075202574957a2b36ee2cd8dd6d987f7f18eacb4
Max Wofford <max@maxwofford.com>
2021-02-08 22:36:37 -0500

    SAVEPOINT

---
+++ api/tasks/user-without-zoom.js
@@ -0,0 +1,7 @@
+// if we find a host in airtable without a zoom account, invite them
+// if we find a host in airtable without a zoom ID, record their zoom ID
+
+const { default: fetch } = require("node-fetch")
+
+module.exports = async (req, res) => {
+}
---
+++ server.js
@@ -0,0 +1,19 @@
+if (process.env.NODE_ENV != 'production') {
+  require('dotenv').config()
+}
+
+const express = require('express')
+const app = express()
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+app.get('/ping', (req, res) => {
+  res.send('pong!')
+})
+
+require('./router')(app)
+
+const port = process.env.PORT || 0
+const listener = app.listen(port, () => {
+  console.log(`Listening on port ${listener.address().port}`)
+})
