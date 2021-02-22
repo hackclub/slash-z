fe1faa1e61d1bd668f53c7bbfe97e117d7515555
Max Wofford <max@maxwofford.com>
2021-02-22 16:16:04 -0500

    Add initial schedule link creation logic

--- api/endpoints/new-schedule-link.js
+++ api/endpoints/new-schedule-link.js
@@ -1,5 +1,20 @@
 const AirBridge = require("../airbridge")
 
 module.exports = async (req, res) => {
-  res.status(200).send('Working on it!')
+  let user = await AirBridge.find('Authed Accounts', { filterByFormula: `{Name}='${req.query.id}'` })
+  if (!user) {
+    // in the future, we'll return an oauth challenge if we can't find a user
+    // with this id. for now, let's just give them an account
+    user = await AirBridge.create('Authed Accounts', {'Name': req.query.id})
+  }
+
+  // let's spice this name creation up in the future too
+  const id = Math.random().toString(36).substring(7)
+  res.status(200).send({id, videoUri: `https://hack.af/z?id=${id}`})
+
+  AirBridge.create('Scheduling Links', {
+    'Name': id,
+    'Creator Slack ID': user.fields['Slack ID'],
+    'Authed Account': [user.id],
+  })
 }
