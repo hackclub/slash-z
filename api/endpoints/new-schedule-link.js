95abcc525549cabeb123d8b1ed16ef1ef015a4b5
Max Wofford <max@maxwofford.com>
2021-02-24 17:33:06 -0500

    Whoops! Set the right oauth url this time

--- api/endpoints/new-schedule-link.js
+++ api/endpoints/new-schedule-link.js
@@ -8,7 +8,8 @@ module.exports = async (req, res) => {
   if (!user.fields['Slack ID']) {
     // No slack ID for this user? they're unauthenticated! Let's return an auth challenge
     const redirectUrl = 'https://hack.af/z/slack-auth'
-    const authUrl = `https://slack.com/oauth/v2/authorize?response_type=code&redirect_uri=${encodeURIComponent(redirectUrl)}&user_scope=identify&client_id=2210535565.1711449950551&state=${user.id}`
+    // const authUrl = `https://slack.com/oauth/v2/authorize?response_type=code&redirect_uri=${encodeURIComponent(redirectUrl)}&user_scope=identify&client_id=2210535565.1711449950551&state=${user.id}`
+    const authUrl = `https://js-slash-z.hackclub.com/auth-start.html?response_type=code&redirect_uri=${encodeURIComponent(redirectUrl)}&user_scope=identify&client_id=2210535565.1711449950551&state=${user.id}`
     return res.json({
       error: 'AUTH',
       authUrl
