4e3eed9992ae6fabf04363d9d4024d35e0c571a9
Max Wofford <max@maxwofford.com>
2021-03-01 17:11:32 -0500

    Switch over to DM'ing the host key to users

---
+++ api/send-host-key.js
@@ -0,0 +1,13 @@
+module.exports = async ({creatorSlackID, hostKey}) => {
+  const slackDM = await fetch('https://slack.com/api/chat.postMessage', {
+    method: 'post',
+    headers: {
+      'Authorization': `Bearer ${process.env.SLACK_BOT_USER_OAUTH_ACCESS_TOKEN}`,
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      channel: creatorSlackID,
+      text: `The host key for your newly created call is ${hostKey}.`
+    })
+  }).then(r => r.json())
+}
