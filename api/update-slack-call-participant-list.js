611a47dacefdafc06cd6517b2155bca1811daf8d
Max Wofford <max@maxwofford.com>
2021-03-05 11:41:45 -0500

    Add context comments explaining #7's solution

--- api/update-slack-call-participant-list.js
+++ api/update-slack-call-participant-list.js
@@ -17,7 +17,14 @@ async function userToAvatar({name, email}) {
 module.exports = async (addOrRemove, callID, zoomParticipant) => {
   const { user_name: name, email, user_id: zoomID } = zoomParticipant
   const user = { }
+  // Why uniquify by name instead of zoomID? the ID zoom gives us changes per join for users under a non-hackclub account.
+  // ex. someone@gmail.com (without a hack club zoom license) joins, leaves, & rejoins the call
+  // their first participant_join event has user ID 16778240, & their second participant_join event has user ID 16790528
   user['external_id'] = name || email || zoomID
+
+  // Slack's calls.participants.remove works just as well if we only provide an
+  // external_id, so let's skip getting their other details to speed up the
+  // request
   if (addOrRemove == 'add') {
     user['avatar_url'] = await userToAvatar({name, email})
     user['display_name'] = name
