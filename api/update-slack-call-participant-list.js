2592717699af385d8178e563a05486b3d0db1694
Max Wofford <max@maxwofford.com>
2021-03-05 11:36:45 -0500

    Only query gravatar when adding participants to slack call

--- api/update-slack-call-participant-list.js
+++ api/update-slack-call-participant-list.js
@@ -18,8 +18,10 @@ module.exports = async (addOrRemove, callID, zoomParticipant) => {
   const { user_name: name, email, user_id: zoomID } = zoomParticipant
   const user = { }
   user['external_id'] = name || email || zoomID
-  user['avatar_url'] = await userToAvatar({name, email})
-  user['display_name'] = name
+  if (addOrRemove == 'add') {
+    user['avatar_url'] = await userToAvatar({name, email})
+    user['display_name'] = name
+  }
   const result = await fetch(`https://slack.com/api/calls.participants.${addOrRemove}`, {
     method: 'post',
     headers: {
