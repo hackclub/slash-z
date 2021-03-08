1cc518ec762f6e3cb1c787eaa9516de81efb2e2f
Max Wofford <max@maxwofford.com>
2021-03-08 16:33:14 -0500

    Add error page for app home page

--- api/slack-app-home-opened.js
+++ api/slack-app-home-opened.js
@@ -7,5 +7,15 @@ module.exports = user => {
       'Content-Type': 'application/json'
     },
     body: JSON.stringify(transcript('appHome.loading', {user}))
-  }).then(r => r.json()).finally(f => console.log(f))
+  }).then(r => r.json()).catch(async err => {
+    await fetch('https://slack.com/api/views.publish', {
+      method: 'post',
+      headers: {
+        'Authorization': `Bearer ${process.env.SLACK_BOT_USER_OAUTH_ACCESS_TOKEN}`,
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(transcript('appHome.error', {err}))
+    })
+    throw err
+  }).finally(f => console.log(f))
 }
