d1f775d0619f1e255b69591c36e386f7cd0b5939
Max Wofford <max@maxwofford.com>
2021-03-08 15:57:56 -0500

    Use transcript to handle home page loading content

--- api/endpoints/slack/events.js
+++ api/endpoints/slack/events.js
@@ -1,5 +1,6 @@
 const ensureSlackAuthenticated = require("../../ensure-slack-authenticated")
 const { default: fetch } = require("node-fetch")
+const { transcript } = require("../../transcript")
 
 module.exports = async (req, res) => {
   return await ensureSlackAuthenticated(req, res, async () => {
@@ -19,44 +20,32 @@ module.exports = async (req, res) => {
               'Authorization': `Bearer ${process.env.SLACK_BOT_USER_OAUTH_ACCESS_TOKEN}`,
               'Content-Type': 'application/json'
             },
-            body: JSON.stringify({
-              user_id: user,
-              view: {
-                type: 'home',
-                blocks: [{
-                  type: "section",
-                  text: {
-                    type: 'mrkdwn',
-                    text: ":beachball: loading..."
-                  }
-                }],
-              }
-            })
+            body: JSON.stringify(transcript('appHome.loading', {user}))
           }).then(r => r.json())
-          const secondRequest = await fetch('https://slack.com/api/views.publish', {
-            method: 'post',
-            headers: {
-              'Authorization': `Bearer ${process.env.SLACK_BOT_USER_OAUTH_ACCESS_TOKEN}`,
-              'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-              user_id: user,
-              view: {
-                type: 'home',
-                blocks: [{
-                  type: 'image',
-                  title: {
-                    type: 'plain_text',
-                    text: ":warning: This part of the mansion is still under construction, please pardon our dust.",
-                    emoji: true
-                  },
-                  image_url: 'https://cloud-oshyn030x-hack-club-bot.vercel.app/0z0q4jqa.gif',
-                  alt_text: 'skeletons dancing'
-                }]
-              }
-            })
-          }).then(r => r.json())
-          console.log(secondRequest)
+          // const secondRequest = await fetch('https://slack.com/api/views.publish', {
+          //   method: 'post',
+          //   headers: {
+          //     'Authorization': `Bearer ${process.env.SLACK_BOT_USER_OAUTH_ACCESS_TOKEN}`,
+          //     'Content-Type': 'application/json'
+          //   },
+          //   body: JSON.stringify({
+          //     user_id: user,
+          //     view: {
+          //       type: 'home',
+          //       blocks: [{
+          //         type: 'image',
+          //         title: {
+          //           type: 'plain_text',
+          //           text: ":warning: This part of the mansion is still under construction, please pardon our dust.",
+          //           emoji: true
+          //         },
+          //         image_url: 'https://cloud-oshyn030x-hack-club-bot.vercel.app/0z0q4jqa.gif',
+          //         alt_text: 'skeletons dancing'
+          //       }]
+          //     }
+          //   })
+          // }).then(r => r.json())
+          // console.log(secondRequest)
           res.status(200).send()
           break
       default:
