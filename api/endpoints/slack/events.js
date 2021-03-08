b10ed2a7476684a94caeb01bb8df41f2e171f5f9
Max Wofford <max@maxwofford.com>
2021-03-08 13:17:13 -0500

    Placeholder app home for testing

--- api/endpoints/slack/events.js
+++ api/endpoints/slack/events.js
@@ -10,6 +10,27 @@ module.exports = async (req, res) => {
       }
       case 'app_home_opened':
         // await displaySlackAppHomeFor(user)
+        const { user } = req.body
+        await fetch('https://slack.com/api/views.publish', {
+          method: 'post',
+          headers: {
+            'Authorization': `Bearer ${process.env.SLACK_BOT_USER_OAUTH_ACCESS_TOKEN}`,
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+            user_id: user,
+            view: {
+              type: 'home',
+              blocks: {
+                type: "section",
+                text: {
+                  type: 'mrkdwn',
+                  text: 'loading...'
+                }
+              },
+            }
+          })
+        })
         break
       default:
         throw new Error(`Unsupported slack event: '${req.body.type}'`)
