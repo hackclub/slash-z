b285bd0415b65841fa0d08fa25c771b36b1ac4df
Max Wofford <max@maxwofford.com>
2021-03-08 13:07:09 -0500

    Don't auto-respond to slack events with 200

--- api/endpoints/slack/events.js
+++ api/endpoints/slack/events.js
@@ -2,7 +2,6 @@ const ensureSlackAuthenticated = require("../../ensure-slack-authenticated")
 
 module.exports = async (req, res) => {
   return await ensureSlackAuthenticated(req, res, async () => {
-    res.status(200).send()
     console.log(`Got verified Slack event of type '${req.body.type}'`)
     switch(req.body.type) {
       case 'url_verification': {
