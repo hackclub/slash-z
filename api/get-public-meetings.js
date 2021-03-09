98f0df7cae8481d9339ff660ea6ebc59a26c26d4
Max Wofford <max@maxwofford.com>
2021-03-09 16:03:40 -0500

    Generalize public meeting fetching

--- api/get-public-meetings.js
+++ api/get-public-meetings.js
@@ -1,7 +1,28 @@
+const { default: fetch } = require("node-fetch")
 const airbridge = require("./airbridge")
+
+async function getParticipantCount(slackCallID) {
+  const callInfo = await fetch('https://slack.com/api/calls.info', {
+    method: 'post',
+    headers: {
+      'Authorization': `Bearer ${process.env.SLACK_BOT_USER_OAUTH_ACCESS_TOKEN}`,
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      id: slackCallID
+    })
+  }).then(r => r.json())
+  return callInfo.call.users.length
+}
 
 module.exports = async function() {
   const filterByFormula = `AND({Status}='OPEN',{Public}=TRUE())`
   const meetings = await airbridge.get('Meetings', {filterByFormula})
-  return meetings
+  return await Promise.all(
+    meetings.map(async m => ({
+      channel: m.fields['Slack Channel ID'],
+      joinUrl: m.fields['Join URL'],
+      participantCount: await getParticipantCount(m.fields['Slack Call ID'])
+    }))
+  )
 }
