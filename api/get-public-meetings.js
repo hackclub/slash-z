02240ed9b14750a3c796764c449b3d5b46ef71e2
Max Wofford <max@maxwofford.com>
2021-03-09 13:54:17 -0500

    Whoops!

--- api/get-public-meetings.js
+++ api/get-public-meetings.js
@@ -1,7 +1,7 @@
 const airbridge = require("./airbridge")
 
 module.exports = async function() {
-  const filterByFormula = `AND({Status}='OPEN',{Public?}=TRUE())`
+  const filterByFormula = `AND({Status}='OPEN',{Public}=TRUE())`
   const meetings = await airbridge.get('Meetings', {filterByFormula})
   return meetings
 }
