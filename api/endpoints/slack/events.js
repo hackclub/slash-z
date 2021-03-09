c6fe0fb18c2afae298121750a385e9acce86fef2
Max Wofford <max@maxwofford.com>
2021-03-09 12:24:43 -0500

    Respond 200 to app_home_opened immediately to prevent retries

--- api/endpoints/slack/events.js
+++ api/endpoints/slack/events.js
@@ -11,14 +11,13 @@ module.exports = async (req, res) => {
       console.log(`Got event of subtype ${req.body.event.type}`)
       switch (req.body.event.type) {
         case 'app_home_opened':
-          console.log(req.body.event)
+          res.status(200).send()
           if (req.body.event.tab === 'home') {
             const { user } = req.body.event
             const result = await slackAppHomeOpened(user)
           } else {
             console.log(`False alarm, this user is opening the '${req.body.event.tab}' tab. I'm ignoring it.`)
           }
-          res.status(200).send()
           break
       default:
         throw new Error(`Unsupported slack event: '${req.body.type}'`)
