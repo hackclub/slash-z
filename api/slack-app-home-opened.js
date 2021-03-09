4629836161fdbdce570090d38fd36ecef17af337
Max Wofford <max@maxwofford.com>
2021-03-09 11:09:35 -0500

    Hardcode error throwing while testing

--- api/slack-app-home-opened.js
+++ api/slack-app-home-opened.js
@@ -1,14 +1,22 @@
 const transcript = require('./transcript')
 const fetch = require('node-fetch')
+
 module.exports = async user => {
-  return await fetch('https://slack.com/api/views.publish', {
-    method: 'post',
-    headers: {
-      'Authorization': `Bearer ${process.env.SLACK_BOT_USER_OAUTH_ACCESS_TOKEN}`,
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(transcript('appHome.loading', {user}))
-  }).then(r => r.json()).catch(async err => {
+  try {
+    await fetch('https://slack.com/api/views.publish', {
+      method: 'post',
+      headers: {
+        'Authorization': `Bearer ${process.env.SLACK_BOT_USER_OAUTH_ACCESS_TOKEN}`,
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(transcript('appHome.loading', {user}))
+    }).then(r => r.json())
+
+    setTimeout(() => {
+      // TODO: actually load in a page to do stuff with
+      throw new Error('Not yet implemented')
+    }, 1000)
+  } catch (err) {
     await fetch('https://slack.com/api/views.publish', {
       method: 'post',
       headers: {
@@ -18,5 +26,5 @@ module.exports = async user => {
       body: JSON.stringify(transcript('appHome.error', {err}))
     })
     throw err
-  }).finally(f => console.log(f))
+  }
 }
