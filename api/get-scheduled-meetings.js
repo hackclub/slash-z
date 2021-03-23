d5669f931d0ba19b586c7868aca92ccd540a1bef
Max Wofford <max@maxwofford.com>
2021-03-23 13:19:20 -0400

    Attempt to fix invalid awaiting in scheduled meeting list

--- api/get-scheduled-meetings.js
+++ api/get-scheduled-meetings.js
@@ -9,19 +9,17 @@ module.exports = async function(user) {
   `
   const links = await airbridge.get('Scheduling Links', {filterByFormula: scheduleLinkFilter})
 
-  const meetings = links.map(async link => {
+  const meetings = await Promise.all(links.map(async link => {
     const meetingFilter = `
     AND(
       {Status}='OPEN',
       NOT({Host Key}=BLANK()),
-      {Scheduling Link}='${link.fields['name']}'
+      {Scheduling Link}='${link.fields['Name']}'
     )
     `
     const meeting = await airbridge.find('Meetings', {filterByFormula: meetingFilter})
     return { meeting, link }
-  })
+  }))
   
-  await Promise.all(meetings)
-
   return meetings
 }
