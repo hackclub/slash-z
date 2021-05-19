2e1c3f0d29cd2b1d682f8789b8af219306bce6aa
Max Wofford <max@maxwofford.com>
2021-05-19 11:17:29 -0400

    Fix a couple minor bugs in #23

--- api/user-is-restricted.js
+++ api/user-is-restricted.js
@@ -2,5 +2,6 @@ const { default: fetch } = require("node-fetch")
 
 module.exports = async function(userID) {
   const userInfo = await fetch(`https://slack.com/api/users.info?token=${process.env.SLACK_BOT_USER_OAUTH_ACCESS_TOKEN}&user=${userID}`).then(r => r.json())
-  return userInfo.user.is_restricted
+  const { is_restricted, is_ultra_restricted } = userInfo.user
+  return is_restricted || is_ultra_restricted
 }
