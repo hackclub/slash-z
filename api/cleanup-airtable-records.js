36fa81d7aeba87dcd85a962096f331e3cc6fab01
Max Wofford <max@maxwofford.com>
2021-06-01 14:10:00 -0400

    Increase record limit for deleting stale events

--- api/cleanup-airtable-records.js
+++ api/cleanup-airtable-records.js
@@ -12,54 +12,54 @@ const limiter = new Bottleneck({
 });
 
 module.exports = async () => {
-  {
-    // step 1: if we're at over 40k log records, let's cleanup old webhook events that aren't related to a meeting 
-    const cutoffSeconds = 60 * 60 * 24 * 3 // 3 days, counted in seconds
-    const filterByFormula = `
-    AND(
-      {Meeting}=BLANK(),
-      DATETIME_DIFF(NOW(),CREATED_TIME())>${cutoffSeconds}
-    )
-    `
-    const events = await airbridge.get('Webhook Events', {filterByFormula, maxRecords: 5000})
-    const limitedJobQueue = events.map(async event => (
-      await limiter.schedule(() => airbridge.destroy('Webhook Events', event.id))
-    ))
-    await Promise.all(limitedJobQueue)
-  }
+  // {
+  //   // step 1: if we're at over 40k log records, let's cleanup old webhook events that aren't related to a meeting 
+  //   const cutoffSeconds = 60 * 60 * 24 * 3 // 3 days, counted in seconds
+  //   const filterByFormula = `
+  //   AND(
+  //     {Meeting}=BLANK(),
+  //     DATETIME_DIFF(NOW(),CREATED_TIME())>${cutoffSeconds}
+  //   )
+  //   `
+  //   const events = await airbridge.get('Webhook Events', {filterByFormula, maxRecords: 5000})
+  //   const limitedJobQueue = events.map(async event => (
+  //     await limiter.schedule(() => airbridge.destroy('Webhook Events', event.id))
+  //   ))
+  //   await Promise.all(limitedJobQueue)
+  // }
 
-  {
-    // step 2: if we have meetings that ended over a month ago, let's cleanup their associated webhook events & pack those into the record itself
-    const cutoffSeconds = 60 * 60 * 24 * 7 // 7 days, counted in seconds
-    const filterByFormula = `
-    AND(
-      {Raw Webhook Events}=BLANK(),
-      {Status}='ENDED',
-      DATETIME_DIFF(NOW(),{Ended At})>${cutoffSeconds}
-    )
-    `
-    const closedMeetings = await airbridge.get('Meetings', { filterByFormula, maxRecords: 1000 })
-    const limitedJobQueue = closedMeetings.map(async closedMeeting =>
-      await limiter.schedule(async () => {
-        console.log('Archiving webhook events for meeting', closedMeeting.id)
-        const eventFormula = `{Meeting}='${closedMeeting.fields['Zoom ID']}'`
-        const events = await airbridge.get('Webhook Events', { filterByFormula: eventFormula })
-        const joinedEvents = events.map(e => JSON.parse(e.fields['Raw Data']))
-        const rawWebhookEvents = JSON.stringify(joinedEvents, null, 2)
-        // https://community.airtable.com/t/what-is-the-long-text-character-limit/1780/4
-        // Airtable only supports lengths of 100,000 characters
-        if (rawWebhookEvents.length <= 100000) {
-          console.log('Updating meeting', closedMeeting.id)
-          await airbridge.patch('Meetings', closedMeeting.id, { 'Raw Webhook Events': rawWebhookEvents })
-          console.log('Done updating meeting', closedMeeting.id)
-        } else {
-          console.log('Not adding webhooks to', closedMeeting.id, '- raw JSON is too long for storing in Airtable')
-          await airbridge.patch('Meetings', closedMeeting.id, { 'Raw Webhook Events Too Long': true })
-        }
-      })
-    )
-    await Promise.all(limitedJobQueue)
-  }
+  // {
+  //   // step 2: if we have meetings that ended over a month ago, let's cleanup their associated webhook events & pack those into the record itself
+  //   const cutoffSeconds = 60 * 60 * 24 * 7 // 7 days, counted in seconds
+  //   const filterByFormula = `
+  //   AND(
+  //     {Raw Webhook Events}=BLANK(),
+  //     {Status}='ENDED',
+  //     DATETIME_DIFF(NOW(),{Ended At})>${cutoffSeconds}
+  //   )
+  //   `
+  //   const closedMeetings = await airbridge.get('Meetings', { filterByFormula, maxRecords: 1000 })
+  //   const limitedJobQueue = closedMeetings.map(async closedMeeting =>
+  //     await limiter.schedule(async () => {
+  //       console.log('Archiving webhook events for meeting', closedMeeting.id)
+  //       const eventFormula = `{Meeting}='${closedMeeting.fields['Zoom ID']}'`
+  //       const events = await airbridge.get('Webhook Events', { filterByFormula: eventFormula })
+  //       const joinedEvents = events.map(e => JSON.parse(e.fields['Raw Data']))
+  //       const rawWebhookEvents = JSON.stringify(joinedEvents, null, 2)
+  //       // https://community.airtable.com/t/what-is-the-long-text-character-limit/1780/4
+  //       // Airtable only supports lengths of 100,000 characters
+  //       if (rawWebhookEvents.length <= 100000) {
+  //         console.log('Updating meeting', closedMeeting.id)
+  //         await airbridge.patch('Meetings', closedMeeting.id, { 'Raw Webhook Events': rawWebhookEvents })
+  //         console.log('Done updating meeting', closedMeeting.id)
+  //       } else {
+  //         console.log('Not adding webhooks to', closedMeeting.id, '- raw JSON is too long for storing in Airtable')
+  //         await airbridge.patch('Meetings', closedMeeting.id, { 'Raw Webhook Events Too Long': true })
+  //       }
+  //     })
+  //   )
+  //   await Promise.all(limitedJobQueue)
+  // }
 
   {
     // step 3: if we have webhook events that have already been packed into their meeting records let's clean them up
@@ -70,7 +70,7 @@ module.exports = async () => {
         {Raw Webhook Events Too Long}=FALSE()
       )
     `
-    const packedMeetings = await airbridge.get('Meetings', { filterByFormula: meetingFormula, maxRecords: 10 })
+    const packedMeetings = await airbridge.get('Meetings', { filterByFormula: meetingFormula, maxRecords: 1000 })
     const limitedJobQueue = packedMeetings.map(async packedMeeting =>
       await limiter.schedule(async () => {
         console.log('Deleting webhook events for already archived meeting', packedMeeting.id)
