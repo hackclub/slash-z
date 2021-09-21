a4dc6495c98755abd68c3c5baa2b35b0af688cb1
Sam Poder <39828164+sampoder@users.noreply.github.com>
2021-09-21 20:12:12 +0800

    Go back on airbridge.js

--- api/airbridge.js
+++ api/airbridge.js
@@ -1,5 +1,5 @@
-const AirtablePlus = require('airtable-plus')
-const Bottleneck = require('bottleneck')
+import AirtablePlus from 'airtable-plus'
+import Bottleneck from 'bottleneck'
 const limiter = new Bottleneck({
   maxConcurrent: 2,
   // minTime: 5000
@@ -12,12 +12,13 @@ const deletionLimiter = new Bottleneck({
 
 const getBaseID = function(baseID = 'Slash-z') {
   return {
-    'Slash-z': 'appILZk0gunMZ1J1i',
+    'Slash-z': 'appuEsdMf6hHXibSh',
     'Operations': 'apptEEFG5HTfGQE7h'
   }[baseID]
 }
 
 const get = async (table, options) => {
+  const ts = Date.now()
   const {base, ...otherOptions} = options
   const baseID = getBaseID(base)
   try {
@@ -26,12 +27,12 @@ const get = async (table, options) => {
       apiKey: process.env.AIRBRIDGE_API_KEY,
       tableName: table,
     })
-   //  console.log(`Airtable GET '${table}' with the following options:`, otherOptions)
+    console.log(`[${ts}] Airtable GET '${table}' with the following options:`, otherOptions)
     const results = await airtable.read(otherOptions)
-   //  console.log(`Found ${results.length} records(s)`)
+    console.log(`[${ts}] Found ${results.length} records(s)`)
     return results
   } catch (err) {
-   //  console.log(err)
+    console.log(err)
   }
 }
 
@@ -41,52 +42,57 @@ const find = async (table, options) => {
 }
 
 const patch = async (table, recordID, fields) => {
+  const ts = Date.now()
   try {
-    // console.log(`Airtable PATCH '${table} ID ${recordID}' with the following fields:`, fields)
+    console.log(`[${ts}] Airtable PATCH '${table} ID ${recordID}' with the following fields:`, fields)
     const airtable = new AirtablePlus({
       baseID: getBaseID('Slash-z'),
       apiKey: process.env.AIRBRIDGE_API_KEY,
       tableName: table,
     })
     const result = await airtable.update(recordID, fields)
+    console.log(`[${ts}] Airtable PATCH successful!`)
     return result
   } catch (err) {
-   //  console.log(err)
+    console.log(err)
   }
 }
 
 const create = async (table, fields) => {
+  const ts = Date.now()
   try {
-   //  console.log(`Airtable CREATE '${table}' with the following fields:`, fields)
+    console.log(`[${ts}] Airtable CREATE '${table}' with the following fields:`, fields)
     const airtable = new AirtablePlus({
       baseID: getBaseID('Slash-z'),
       apiKey: process.env.AIRBRIDGE_API_KEY,
       tableName: table,
     })
-    const results = await airtable.create(fields)
-   //  console.log('Airtable created my record with these fields:', {results})
-    return results
+    const result = await airtable.create(fields)
+    console.log(`[${ts}] Airtable created my record with id: ${result.id}`)
+    return result
   } catch (err) {
-   //  console.log(err)
+    console.log(err)
   }
 }
 
 const destroy = async (table, id) => {
+  const ts = Date.now()
   const airtable = new AirtablePlus({
     baseID: getBaseID('Slash-z'),
     apiKey: process.env.AIRBRIDGE_API_KEY,
     tableName: table,
   })
   try {
-   //  console.log(`Airtable DELETE '${table}' RECORD '${id}'`)
+    console.log(`[${ts}] Airtable DELETE '${table}' RECORD '${id}'`)
     const results = await airtable.delete(id)
+    console.log(`[${ts}] Airtable deletion successful on '${table}' table, record '${id}'!`)
     return results
   } catch (err) {
-   //  console.log(err)
+    console.log(err)
   }
 }
 
-module.exports = {
+export default {
   get: (...args) => limiter.schedule(() => get(...args)),
   find,
   patch: (...args) => limiter.schedule(() => patch(...args)),
