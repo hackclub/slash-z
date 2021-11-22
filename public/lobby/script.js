b2bf7f7601716767f1e9c8aa6e04f5d1078df06b
YodaLightsabr <76178582+YodaLightsabr@users.noreply.github.com>
2021-11-21 22:59:12 -0800

    Update script.js

--- public/lobby/script.js
+++ public/lobby/script.js
@@ -38,3 +38,20 @@ const validate = () => {
     input.value = input.value.split('').filter(char => parseInt(char) == char).join('');
     if (input.value.length > 6) input.value = input.value.substring(0, 6);
 }
+
+setInterval(() => {
+    // Check if call is ready to join
+    fetch('/api/endpoints/join-meeting/', {
+        method: "POST",
+        body: JSON.stringify({
+            meetingID: meetingID
+        })
+    }).then(response => response.json()).then(json => {
+        if (json.success == true) {
+            $`p.lead`.innerText = 'Joining meeting...';
+            $`h3.eyebrow`.innerText = 'Starting Now';
+            // Update the UI before redirecting
+            window.location.href = json.callLink;
+    });
+}, 5000); // Refresh every 5 seconds, could be changed later or replaced with a websocket connection
+            
