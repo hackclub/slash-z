8face23387907c4ec1f7680c4de74e545b8cd939
Charlie Welsh <charlie@allotrope.dev>
2022-04-17 00:05:37 -0700

    Add Slack verification for Hack Night
    Add a redirect to the Slack OAuth stuff for Hack Night.

--- api/endpoints/slack-auth.js
+++ api/endpoints/slack-auth.js
@@ -1,18 +1,39 @@
 import Prisma from '../prisma.js'
 import fetch from 'node-fetch'
+import findOrCreateMeeting from "../find-or-create-meeting.js"
 
 export default async (req, res) => {
-  const {code, state: recordID} = req.query
-  const user = await Prisma.find('authedAccount', recordID)
-
-  if (user) {
-    const tokenUrl = 'https://slack.com/api/oauth.v2.access' +
+  const {code, state: recordIDData} = req.query
+  
+  const {userID, meetingID} = JSON.parse(Buffer.from(decodeURIComponent(recordIDData), "base64").toString())
+  
+  // Generate the token request
+  const tokenUrl = 'https://slack.com/api/oauth.v2.access' +
                       `?code=${code}` +
                       `&client_id=${process.env.SLACK_CLIENT_ID}` +
                       `&client_secret=${process.env.SLACK_CLIENT_SECRET}` +
                       `&redirect_uri=${encodeURIComponent('https://hack.af/z/slack-auth')}`
+  
+  if (meetingID === "1vu13b") { // Hack Night!
     const slackData = await fetch(tokenUrl, {method: 'post'}).then(r => r.json())
-    await Prisma.patch('authedAccount', recordID, { slackID: slackData['authed_user']['id'] })
+    
+    // Check if the authed user actually exists
+    if (slackData?.authed_user?.id === null) {
+      res.redirect('/auth-error.html')
+      return
+    }
+    
+    // Dynamically generate the meeting ID for future flexibility
+    let meeting = await findOrCreateMeeting(meetingID)
+    res.redirect(meeting.joinURL)
+    return
+  }
+  
+  const user = await Prisma.find('authedAccount', userID)
+
+  if (user) {
+    const slackData = await fetch(tokenUrl, {method: 'post'}).then(r => r.json())
+    await Prisma.patch('authedAccount', userID, { slackID: slackData['authed_user']['id'] })
     // res.status(200).send('It worked! You can close this tab now')
     res.redirect('/auth-success.html')
   } else {
