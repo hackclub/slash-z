1b095e3aa34a6e53381359ca63a6358a6601e8cf
Charlie Welsh <charlie@allotrope.dev>
2022-04-17 18:44:48 -0700

    Fix forgotten state object conversion

--- api/endpoints/new-schedule-link.js
+++ api/endpoints/new-schedule-link.js
@@ -8,8 +8,11 @@ export default async (req, res) => {
   if (!user.slackID) {
     // No slack ID for this user? they're unauthenticated! Let's return an auth challenge
     const redirectUrl = 'https://hack.af/z/slack-auth'
-    // const authUrl = `https://slack.com/oauth/v2/authorize?response_type=code&redirect_uri=${encodeURIComponent(redirectUrl)}&user_scope=identify&client_id=2210535565.1711449950551&state=${user.id}`
-    const authUrl = `https://js-slash-z.hackclub.com/auth-start.html?response_type=code&redirect_uri=${encodeURIComponent(redirectUrl)}&user_scope=identify&client_id=2210535565.1711449950551&state=${user.id}`
+    
+    const state = { userID: user.id }
+    const stateString = encodeURIComponent(Buffer.from(JSON.stringify(state), "utf8").toString("base64"))
+    
+    const authUrl = `https://js-slash-z.hackclub.com/auth-start.html?response_type=code&redirect_uri=${encodeURIComponent(redirectUrl)}&user_scope=identify&client_id=2210535565.1711449950551&state=${stateString}`
     return res.json({
       error: 'AUTH',
       authUrl
