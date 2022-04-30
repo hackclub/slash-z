72e265f04581224212fdd60db491ea5e838f8a3e
YodaLightsabr <76178582+YodaLightsabr@users.noreply.github.com>
2022-04-29 18:50:18 -0700

    Update private channel implementation

--- api/is-public-slack-channel.js
+++ api/is-public-slack-channel.js
@@ -16,7 +16,13 @@ export default async function(channelID) {
         'Authorization': `Bearer ${process.env.SLACK_BOT_USER_OAUTH_ACCESS_TOKEN}`
       }
     }).then(r => r.json()).then(channelInfo => {
-      isPublic = isPublic && (channelInfo.ok && !channelInfo.channel['is_private'])
+      isPublic = isPublic && (
+        channelInfo.ok &&
+        !channelInfo.channel['is_private'] &&
+        !channelInfo.channel['is_im'] &&
+        !channelInfo.channel['is_mpim'] &&
+        !channelInfo.channel['is_group']
+      )
     }),
     // check Operations airtable to see if this channel isn't a club channel
     airbridge.find('Clubs', {base: 'Operations', filterByFormula: `{Slack Channel ID}='${channelID}'`}).then(c => {
