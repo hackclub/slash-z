e20984f20b2f2e3f9ef5ade16ac7dff29b8bb7ed
Yoda <yoda@yodacode.xyz>
2022-12-18 19:27:31 -0800

    Refactor bookmark code and automatically set key

--- api/hack-night.js
+++ api/hack-night.js
@@ -1,19 +1,18 @@
 import fetch from "node-fetch";
+import { currentTimeHash } from "./time-hash.js";
 
-export async function fetchParticipantNumber () {
+export async function fetchBookmarkTitle () {
   const response = await fetch('https://slack.com/api/bookmarks.list?channel_id=C0JDWKJVA', {
     headers: {
       Authorization: `Bearer ${process.env.SLACK_BOT_USER_OAUTH_ACCESS_TOKEN}`
     }
   });
   const json = await response.json();
-  // console.log(json);
   const { title } = json.bookmarks.filter(bookmark => bookmark.id === "Bk027L39LR9A")[0];
-  const number = +(title.split('').filter(char => !isNaN(char)).join('') || '0');
-  return number;
+  return title;
 }
 
-export async function setParticipantNumber (number) {
+export async function setBookmarkTitle (title) {
   const response = await fetch('https://slack.com/api/bookmarks.edit', {
     method: 'POST',
     headers: {
@@ -24,17 +23,25 @@ export async function setParticipantNumber (number) {
       bookmark_id: 'Bk027L39LR9A',
       channel_id: 'C0JDWKJVA',
       emoji: ':zoom:',
-      link: 'https://hack.af/night',
-      title: 'Join Hack Night! 👤 ' + number
+      link: 'https://hack.af/night?key=' + currentTimeHash(),
+      title
     })
   });
   const json = await response.json();
-  // console.log(json);
   return json;
 }
 
-export default async function hackNightStats (event, meeting, payload) {
+export async function fetchParticipantNumber () {
+  const title = await fetchBookmarkTitle();
+  const number = +(title.split('').filter(char => !isNaN(char)).join('') || '0');
+  return number;
+}
 
+export async function setParticipantNumber (number) {
+  return await setBookmarkTitle('Join Hack Night! 👤 ' + number);
+}
+
+export default async function hackNightStats (event, meeting, payload) {
   switch (event) {
     case 'meeting.ended':
       await setParticipantNumber(0);
