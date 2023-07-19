ea435fc5812938954ca13dd27b73c6016c66ef5a
JosiasAurel <ndjosiasaurel@gmail.com>
2023-07-19 17:29:11 +0100

    add url for quickly accessing zoom from staging

--- api/endpoints/new-schedule-link.js
+++ api/endpoints/new-schedule-link.js
@@ -8,25 +8,28 @@ export default async (req, res) => {
   }
   if (!user.slackID) {
     // No slack ID for this user? they're unauthenticated! Let's return an auth challenge
-    const redirectUrl = process.env.NODE_ENV === "production" ? 'https://hack.af/z/slack-auth' : "https://slash-z-staging-1ae8b1c9e24a.herokuapp.com/api/endpoints/slack-auth";
+    const redirectUrl = 'https://hack.af/z/slack-auth'
     
     const state = { userID: user.id }
     console.log({state})
     const stateString = encodeURIComponent(Buffer.from(JSON.stringify(state), "utf8").toString("base64"))
     
-    const baseUrl = process.env.NODE_ENV === "production" ? "https://js-slash-z.hackclub.com" : "https://slash-z-staging-1ae8b1c9e24a.herokuapp.com";
-    const authUrl = `${baseUrl}/auth-start.html?response_type=code&redirect_uri=${encodeURIComponent(redirectUrl)}&user_scope=identify&client_id=${process.env.SLACK_CLIENT_ID}&state=${stateString}`
+    const authUrl = `https://js-slash-z.hackclub.com/auth-start.html?response_type=code&redirect_uri=${encodeURIComponent(redirectUrl)}&user_scope=identify&client_id=${process.env.SLACK_CLIENT_ID}&state=${stateString}`
     return res.json({
       error: 'AUTH',
       authUrl
     })
   }
 
+  // should open a meeting using
+  // <staging-app-url>/api/endpoints/schedule-link?id=9whz6
+
   // let's spice this name creation up in the future too
   const id = Math.random().toString(36).substring(7)
   res.json({id,
     videoUri: `https://hack.af/z-join?id=${id}`,
-    moreUri: `https://hack.af/z-phone?id=${id}`
+    moreUri: `https://hack.af/z-phone?id=${id}`,
+    stagingVideoUri: process.env.NODE_ENV === "staging" ? `https://slash-z-staging-1ae8b1c9e24a.herokuapp.com/api/endpoints/schedule-link?id=${id}` : null 
   })
 
   Prisma.create('schedulingLink', {
