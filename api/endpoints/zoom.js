ebf4b49d3043c9b418d998fc2786a1cf7ab88238
graham <graham@hackclub.com>
2023-08-18 11:26:05 -0400

    Support new webhook integration using OAuth srv<>srv (#150)
    * listen to url_validation webhook
    
    * fix: undefined zoomCallId
    
    * get meeting id if defined
    
    * catch zoomCallId
    
    * optional convert meeting to string
    
    * minot changes
    
    * Refactor file for additional clarity and simplicity
    
    * Fixing async issue
    
    * Split events that require meeting from those that don't
    
    * Fix reference
    
    * Fix typo
    
    * Fix invalid reference
    
    * modify logic to not try persisting webhook events with no meeting associated
    
    * respond to url validation webhook challenge
    
    * Use correct environment variable
    
    * fix reference
    
    * print req and response:
    
    * reformat
    
    * respond with json appropriately
    
    * Modify who sets the status code
    
    * Always return with something, else we get request timeouts
    
    * we were not sending responses
    
    * use zoomID instead of id
    
    ---------
    
    Co-authored-by: JosiasAurel <ndjosiasaurel@gmail.com>

--- api/endpoints/zoom.js
+++ api/endpoints/zoom.js
@@ -4,66 +4,96 @@ import ensureZoomAuthenticated from "../ensure-zoom-authenticated.js"
 import updateSlackCallParticipantList from "../update-slack-call-participant-list.js"
 import slackAppHomeOpened from "../slack-app-home-opened.js"
 import hackNightStats from "../hack-night.js"
+import crypto from "crypto"
 
+async function getAssociatedMeeting(req) {
+  try {
+      const meetingId = req.body.payload.object.id;
+      return await Prisma.find('meeting', { where: { zoomID: meetingId }, include: { schedulingLink: true } })
+  } catch {
+    return null
+  }
+}
+
+async function persistWebhookEventsIfNecessary(req, meeting) {
+  if (!meeting)
+    return
+
+  await Prisma.create('webhookEvent', {
+    timestamp: new Date(req.body.event_ts),
+    eventType: req.body.event,
+    rawData: JSON.stringify(req.body, null, 2),
+    meeting: { connect: { id: meeting.id } }
+  })
+}
+
+async function handleSpecialHackNightLogic(req, meeting) {
+  const isHackNight = meeting.schedulingLink?.name === "1vu13b";
+
+  if (isHackNight) 
+    await hackNightStats(req.body.event, meeting, req.body.payload);
+}
+
+// Zoom will sometimes send duplicate events, drop an event, or send an
+async function handleEvent(req, res, meeting) {
+  // First, handle events that do not require a meeting
+  switch(req.body.event) {
+    case 'endpoint.url_validation':    
+      const encryptedToken = crypto.createHmac('sha256', process.env.ZOOM_WEBHOOK_SECRET_TOKEN).update(req.body.payload.plainToken).digest("hex");
+      const response = {
+        plainToken: req.body.payload.plainToken,
+        encryptedToken: encryptedToken
+      }  
+      console.log(JSON.stringify(req.body))
+      console.log(JSON.stringify(response))
+      res.status(200).json(response)
+      return;
+  }
+
+  if (!meeting) {
+    console.log('Meeting not found, skipping...')
+    res.status(404).send("That meeting does not exist")
+    return
+  }
+
+  await handleSpecialHackNightLogic(req, meeting)
+
+  switch (req.body.event) {
+    case 'meeting.ended':
+      await Prisma.create('customLogs', { text: 'zoom_end_meeting_webhook', zoomCallId: meeting.zoomID || "undefined" })
+      console.log('Attempting to close call w/ ID of', )
+      await closeZoomCall(meeting.zoomID, false)
+      break
+
+      case 'meeting.participant_joined':
+      console.log('triggered!')
+      await updateSlackCallParticipantList('add', meeting.slackCallID, req.body.payload.object.participant)
+      break
+
+    case 'meeting.participant_left':
+      await updateSlackCallParticipantList('remove', meeting.slackCallID, req.body.payload.object.participant)
+      break
+
+    case 'recording.completed':
+      await slackAppHomeOpened(meeting.creatorSlackID, false)
+      break
+
+    default:
+      console.log(`Recieved '${req.body.event}' event from Zoom webhook, which I don't know how to process... Skipping`)
+      console.log(`Just in case, here's the info:`)
+      console.log(JSON.stringify(req.body, null, 2))
+      res.status(415).send("Unsupported webhook event")
+      return;
+  }
+
+  res.status(200).send('Success!')
+}
 
 export default async (req, res) => {
   return await ensureZoomAuthenticated(req, res, async () => {
     console.log(`Recieved Zoom '${req.body.event}' webhook...`)
-
-    // Zoom will sometimes send duplicate events, drop an event, or send an
-    // event delayed (in testing I found up to 30 minutes late)
-
-    // Let's lookup our webhook event to see if we already got this event.
-    const zoomCallID = req.body.payload.object.id
-
-    const meeting = await Prisma.find('meeting', { where: { zoomID: zoomCallID.toString() }, include: { schedulingLink: true } })
-
-    const fields = {
-      timestamp: new Date(req.body.event_ts),
-      eventType: req.body.event,
-      rawData: JSON.stringify(req.body, null, 2),
-    }
-
-    if (meeting) {
-      fields.meeting = { connect: { id: meeting.id } }
-    }
-
-    await Prisma.create('webhookEvent', fields)
-
-    if (!meeting) {
-      console.log('Meeting not found, skipping...', zoomCallID)
-      return
-    }
-    
-    const isHackNight = meeting?.schedulingLink?.name === "1vu13b";
-
-    if (isHackNight) hackNightStats(req.body.event, meeting, req.body.paylod);
-
-    switch (req.body.event) {
-      case 'meeting.ended':
-        await Prisma.create('customLogs', { text: 'zoom_end_meeting_webhook', zoomCallId: zoomCallID || "undefined" })
-        console.log('Attempting to close call w/ ID of', zoomCallID)
-        return await closeZoomCall(zoomCallID, false)
-        break
-      case 'meeting.participant_joined':
-        console.log('triggered!')
-        return await updateSlackCallParticipantList('add', meeting.slackCallID, req.body.payload.object.participant)
-        break
-      case 'meeting.participant_left':
-        return await updateSlackCallParticipantList('remove', meeting.slackCallID, req.body.payload.object.participant)
-        break
-      case 'recording.started':
-        // return await postSlackCallThread(meeting)
-        // ^ not implemented yet
-        break
-      case 'recording.completed':
-        return await slackAppHomeOpened(meeting.creatorSlackID, false)
-        break
-      default:
-        console.log(`Recieved '${req.body.event}' event from Zoom webhook, which I don't know how to process... Skipping`)
-        console.log(`Just in case, here's the info:`)
-        console.log(JSON.stringify(req.body, null, 2))
-        break
-    }
+    const meeting = await getAssociatedMeeting(req);
+    await persistWebhookEventsIfNecessary(req, meeting);
+    await handleEvent(req, res, meeting);
   })
 }
--- api/ensure-zoom-authenticated.js
+++ api/ensure-zoom-authenticated.js
@@ -3,7 +3,6 @@ export default async (req, res, callback) => {
   const secret = process.env.ZOOM_VERIFICATION_TOKEN
   if (!secret || req.header('authorization') == secret) {
     await callback()
-    res.status(200).send('Success!')
   } else {
     res.status(403).send('Missing/invalid Zoom verification token')
   }
