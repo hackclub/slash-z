fee04d76f1092950eea960be002a505b41b800ea
JosiasAurel <ndjosiasaurel@gmail.com>
2023-08-21 17:53:02 +0100

    authenticate webhook requests

--- api/ensure-zoom-authenticated.js
+++ api/ensure-zoom-authenticated.js
@@ -1,9 +1,20 @@
+import crypto from "crypto";
 // this is a helper method to make sure the zoom request we get is authentic
 export default async (req, res, callback) => {
-  const secret = process.env.ZOOM_VERIFICATION_TOKEN
-  if (!secret || req.header('authorization') == secret) {
-    await callback()
+  if (isZoomAuthenticRequest(req)) {
+    await callback();
   } else {
-    res.status(403).send('Missing/invalid Zoom verification token')
+    res.status(403).send('Unauthorized sender');
   }
+}
+
+function isZoomAuthenticRequest(req) {
+  const message = `v0:${req.headers['x-zm-request-timestamp']}:${JSON.stringify(req.body)}`;
+  const hashForVerify = crypto.createHmac('sha256', process.env.ZOOM_WEBHOOK_SECRET_TOKEN).update(message).digest('hex');
+  const signature = `v0=${hashForVerify}`;
+
+  if (req.headers['x-zm-signature'] === signature) {
+    return true;
+  }
+  return false;
 }
