bdf2348b22400261a01b5486a1cdab9216ec6ae2
JosiasAurel <ndjosiasaurel@gmail.com>
2024-02-07 23:27:00 +0100

    cast meeting id to string
    after investigation, i realised slash-z would trow errors like \n PrismaClientValidationError: Argument zoomID: Got invalid value 88313345283 on prisma.findFirstMeeting. Provided Int, expected StringFilter or String: \nSo, passing the meeting id as a string should resolve the issue

--- api/endpoints/zoom.js
+++ api/endpoints/zoom.js
@@ -9,7 +9,7 @@ import crypto from "crypto"
 async function getAssociatedMeeting(req) {
   try {
       const meetingId = req.body.payload.object.id;
-      return await Prisma.find('meeting', { where: { zoomID: meetingId }, include: { schedulingLink: true } })
+      return await Prisma.find('meeting', { where: { zoomID: meetingId.toString() }, include: { schedulingLink: true } })
   } catch {
     return null
   }
