a26e055dcc485f5b326cefec4deb053bc8383654
Kartikey S. Chauhan <skartikey314@gmail.com>
2024-05-27 19:17:00 +0530

    feat: allows a user to fetch host code after the meeting has been created

---
+++ api/endpoints/slack/host-code.js
@@ -0,0 +1,53 @@
+import Prisma from "../../prisma.js";
+import userIsRestricted from "../../user-is-restricted.js";
+import channelIsForbidden from "../../channel-is-forbidden.js";
+import transcript from '../../transcript.js';
+import fetch from 'node-fetch';
+
+const sendEphemeralMessage = (url, text) => {
+  return fetch(url, {
+    method: 'post',
+    headers: {
+      'Authorization': `Bearer ${process.env.SLACK_BOT_USER_OAUTH_ACCESS_TOKEN}`,
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      response_type: 'ephemeral',
+      text: text
+    })
+  });
+};
+
+export default async (req, res) => {
+  const { user_id, response_url, channel_id, text } = req.body;
+
+  if (await userIsRestricted(user_id)) {
+    return sendEphemeralMessage(response_url, transcript('errors.userIsRestricted'));
+  }
+
+  if (channelIsForbidden(channel_id)) {
+    return sendEphemeralMessage(response_url, transcript('errors.channelIsForbidden'));
+  }
+
+  if (!text) {
+    return sendEphemeralMessage(response_url, transcript('errors.emptyHostCode'));
+  }
+
+  const meeting = await Prisma.find('meeting', { where: { zoomID: text } });
+
+  if (!meeting) {
+    return sendEphemeralMessage(response_url, 'Unable to retrieve the host code. Please check the code and remove any Markdown formatting.');
+  }
+
+  if (meeting.endedAt) {
+    return sendEphemeralMessage(response_url, 'Unable to retrieve the host code for a concluded meeting.');
+  }
+
+  if (meeting.creatorSlackID !== user_id) {
+    return sendEphemeralMessage(response_url, '_You can only retrieve the host code for meetings you created._');
+  }
+  
+  if (meeting.creatorSlackID === user_id) {
+    return sendEphemeralMessage(response_url, `_Your meeting code is: *${meeting.hostKey}*_`);
+  }
+};
--- api/endpoints/slack/index.js
+++ api/endpoints/slack/index.js
@@ -1,6 +1,7 @@
 import ensureSlackAuthenticated from "../../ensure-slack-authenticated.js"
 import slashZ from './slash-z.js'
 import slashZRooms from './slash-z-rooms.js'
+import hostCode from "./host-code.js"
 import isProd from "../../../isprod.js"
 
 export default async (req, res) => {
@@ -15,6 +16,9 @@ export default async (req, res) => {
         break
       case isProd ? '/z-rooms' : '/test-z-rooms':
         await slashZRooms(req, res)
+        break
+      case isProd ? 'z-code' : '/test-z-code':
+        await hostCode(req, res)
         break
       default:
         throw new Error(`Unsupported slash command: '${req.body.command}'`)
